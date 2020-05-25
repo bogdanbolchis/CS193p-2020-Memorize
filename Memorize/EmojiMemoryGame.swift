164b8680b775b10aa55933d4f61dc5872d5dbcc7
bogdanb <bolchisbolchis@gmail.com>
2020-05-25 21:34:45 +0200

    Lecture 2

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Bogdan Bolchis on 24/05/2020.
+//  Copyright © 2020 Bogdan Bolchis. All rights reserved.
+//
+
+import Foundation
+
+class EmojiMemoryGame {
+    private(set) var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    class func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
